bc21b1727ce3bedcfc49af67adae602fe55eb7f7
Taraansh <taraanshnandwani123@gmail.com>
2023-05-06 23:51:13 +0530

    Removed a model

--- appointment/models.py
+++ appointment/models.py
@@ -8,10 +8,7 @@ STATUS_CHOICES = [
         ('Confirmed', 'Confirmed'),
         ('Cancelled', 'Cancelled'),
     ]
-AVAILABILITY_CHOICES = [
-        ('available', 'Available'),
-        ('not vailable', 'Not Available'),
-    ]
+
 TIMING_CHOICES = [
     ('Morning', 'Morning'),
     ('Evening', 'Evening'),
@@ -20,7 +17,7 @@ class Appointment(models.Model):
     appointment_id = models.AutoField(primary_key=True)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     doctor = models.CharField(max_length=50, default='')
-    date = models.DateField(auto_now_add=False)
+    date = models.DateField(auto_now_add=True)
     timing = models.CharField(max_length=20, choices=TIMING_CHOICES, default='morning')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='requested')
     reason = models.TextField(blank=True, null=True)
@@ -30,11 +27,3 @@ class Appointment(models.Model):
     def __str__(self):
         return f'{self.patient} - {self.date}'
 
-
-class DoctorAvailability(models.Model):
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='availabilities')
-    morning_available = models.CharField(max_length=20, choices=AVAILABILITY_CHOICES, default='available')
-    evening_available = models.CharField(max_length=20, choices=AVAILABILITY_CHOICES, default='available')
-
-    def __str__(self):
-        return self.doctor.doctor_name
