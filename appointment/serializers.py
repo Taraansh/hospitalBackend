888926fceeabe7843a0d62db3b074e860fcaa134
Taraansh <taraanshnandwani123@gmail.com>
2023-05-06 23:51:31 +0530

    Removed serializer

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from appointment.models import Appointment, DoctorAvailability
+from appointment.models import Appointment
 
 class AppointmentSerializer(serializers.ModelSerializer):
     patient_name = serializers.SerializerMethodField()
@@ -12,13 +12,3 @@ class AppointmentSerializer(serializers.ModelSerializer):
     def get_patient_name(self, obj):
         return f"{obj.patient.patient_first_name} {obj.patient.patient_last_name}"
 
-
-class DoctorAvailabilitySerializer(serializers.ModelSerializer):
-    doctor_name = serializers.SerializerMethodField()
-    class Meta:
-        model = DoctorAvailability
-        # fields = "__all__"
-        fields= ('id', 'morning_available', 'evening_available', 'doctor_name')
-
-    def get_doctor_name(self, obj):
-        return f"{obj.doctor.doctor_name}"
